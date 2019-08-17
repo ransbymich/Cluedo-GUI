74cf9fd5af1c14c582898e3c4a1a0c22a5b8d99e
TimSalisbury <38494494+TimSalisbury@users.noreply.github.com>
2019-08-18 11:39:39 +1200

    Added dialog if less than three players is selected, creation of canvas class for board drawing

---
+++ src/Cluedo/GUI/Canvas.java
@@ -0,0 +1,20 @@
+package Cluedo.GUI;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Canvas extends JPanel {
+
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 400;
+
+    public Canvas() {
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+
+    }
+}
--- src/Cluedo/GUI/GUI.java
+++ src/Cluedo/GUI/GUI.java
@@ -1,12 +1,19 @@
 package Cluedo.GUI;
 
+import Cluedo.Helpers.Type;
+
 import javax.swing.*;
+import java.util.List;
 
 public class GUI extends JFrame {
 
-
     public GUI(){
-        new CharacterSelection((i)->{});
+        new CharacterSelection(this::initalize);
+    }
+
+
+    private void initalize(List<Cluedo.Helpers.Type> players){
+
     }
 
     public static void main(String[] args) {
